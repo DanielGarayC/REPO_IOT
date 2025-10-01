ea700ce36bcd96f6e42b58ed09adfe9cfc7d0863
JJVA07 <a20203266@pucp.edu.pe>
2025-10-01 12:22:18 -0500

    ya compila la pagina ahora

--- app/app.py
+++ app/app.py
@@ -28,4 +28,4 @@ def health():
     return {"status": "ok"}, 200
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
